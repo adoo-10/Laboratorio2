800676fe0f6087d69e4a3ac26f31b75e170e9324
krkmo10 <cm0252032021@unab.edu.sv>
2021-10-11 16:18:33 -0500

    cc

--- Laboratorio2/Laboratorio2/FrmGuardarInformacion.Designer.cs
+++ Laboratorio2/Laboratorio2/FrmGuardarInformacion.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Laboratorio2
 {
-    partial class Form1
+    partial class FrmGuardarInformacion
     {
         /// <summary>
         /// Variable del diseñador necesaria.
--- Laboratorio2/Laboratorio2/FrmGuardarInformacion.cs
+++ Laboratorio2/Laboratorio2/FrmGuardarInformacion.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Laboratorio2
 {
-    public partial class Form1 : Form
+    public partial class FrmGuardarInformacion : Form
     {
-        public Form1()
+        public FrmGuardarInformacion()
         {
             InitializeComponent();
         }
